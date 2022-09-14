358181880917a9405d7e695ce0badfc3ad1017e6
Atiqur Rahman <atiqur.india@gmail.com>
2022-09-14 13:34:36 +0530

    Edit log message

--- scripts/withdraw.js
+++ scripts/withdraw.js
@@ -3,7 +3,7 @@ const { getNamedAccounts, ethers } = require("hardhat")
 async function main() {
     const { deployer } = await getNamedAccounts()
     const fundMe = await ethers.getContract("FundMe", deployer)
-    console.log("Funding...")
+    console.log("Withdrawing fund...")
     const transactionResponse = await fundMe.withdraw()
     await transactionResponse.wait(1)
     console.log("Got it back!")
